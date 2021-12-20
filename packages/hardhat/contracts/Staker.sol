e22fdffc87a1e386fd18817d29ed73f1a97257d3
Sean Paterson <seanpaterson_25@msn.com>
2021-12-20 09:39:17 +0000

    Up to check point 4:
    Implement all the basics of the contact.
    
    Some changes compared to guidance:
    - Only allow to stake if before deadline
    - Stake updates a variable that says if execution is possible (instead
      checking the first time execute is run. Otherwise people could keep
      adding after deadline as long as no one tries to execute)
    - Withdraw / execute only callable if after deadline

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,31 +1,84 @@
 pragma solidity 0.8.4;
+//SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
 
-  ExampleExternalContract public exampleExternalContract;
+    // Point at which the contract can be executed or withdrawn
+    uint256 contractDeadline = block.timestamp + 60 seconds;
+    // Has balance past threshold before contractDeadline
+    bool ableToExecute = false;
 
-  constructor(address exampleExternalContractAddress) public {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    ExampleExternalContract public exampleExternalContract;
+    // Upper bound that must be hit before all parties can execute the external contract
+    uint256 public constant threshold = 2 ether;
+    // The amount staked by each participant
+    mapping(address => uint256) public stakedBalances;
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    // Callback that an amount has been staked
+    event Stake(address,uint256);
+
+    constructor(address exampleExternalContractAddress) public {
+        exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    }
+
+    // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+    function stake() public payable {
+        require(this.timeLeft() != 0, "No time left on contract");
+        stakedBalances[msg.sender] = stakedBalances[msg.sender] + msg.value;
+
+        if (address(this).balance >= threshold) {
+            ableToExecute = true;
+        }
+
+        emit Stake(msg.sender, msg.value);
+    }
 
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    // If contract balance is passed the threshold before the deadline then execute external contract
+    function execute() external pastTimeThreshold {
+        require(ableToExecute, "Staked balance has not passed threshold");
+        require(!exampleExternalContract.completed(), "Contract has already been executed");
+
+        exampleExternalContract.complete{value: address(this).balance}();
+    }
 
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    function withdraw() external pastTimeThreshold {
+        uint256 sendersBalance = stakedBalances[msg.sender];
+        require(sendersBalance != 0, "User has no balance to withdraw");
+        stakedBalances[msg.sender] = 0;
+
+        (bool success, ) = msg.sender.call{value: sendersBalance}("");
+        require(success, "Failed to withdraw Ether");
+    }
 
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    function timeLeft() public view returns (uint256) {
+        if (block.timestamp >= contractDeadline) {
+            return 0;
+        }
 
+        return contractDeadline - block.timestamp;
+    }
 
-  // Add the `receive()` special function that receives eth and calls stake()
+    // TODO: Delete
+    function inbetweenStake() external payable {
+        stake();
+    }
 
+    // Fallback function for any eth sent to contract (special function so does not need function keyword)
+    receive() external payable {
+        stake(); // If you use this.stake{value: msg.value}() then msg.sender will be address(this)
+    }
 
+    // Ensures function is only called after contractDeadline
+    modifier pastTimeThreshold() {
+        require(this.timeLeft() == 0, "Contract has not ended yet");
+        _;
+    }
 }
